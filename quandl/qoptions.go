898a88bb44c284326d3af3023ee851361253e2f7
xavier268 <xavier268fedora31onHPEnvy>
2020-01-15 16:47:00 +0100

    option api

--- quandl/qoptions.go
+++ quandl/qoptions.go
@@ -1,15 +1,73 @@
 package quandl
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // QOtions are options that can be
-// defined when constructing a new Q.
+// provided when constructing a new Q.
 type QOption func(*Q)
 
-func OptionStartDate(when time.Time) {
-	// TODO
+// OPtionStartDate sets the start date.
+func OptionStartDate(when time.Time) QOption {
+	return func(q *Q) {
+		q.query.Set("start_date", when.Format(layout))
+	}
 }
 
-func OptionEndDate(when time.Time) {
-	// TODO
+// OptionEndDate sets the end date.
+func OptionEndDate(when time.Time) QOption {
+	return func(q *Q) {
+		q.query.Set("end_date", when.Format(layout))
+	}
+}
+
+// OptionOrderAsc ascending order.
+var OptionOrderAsc QOption = func(q *Q) {
+	q.query.Set("order", "asc")
+}
+
+// OptionOrderDesc descending order.
+var OptionOderDesc QOption = func(q *Q) {
+	q.query.Set("order", "desc")
+}
+
+// OptionFrequency sets the sampling frequency.
+func OptionFrequency(freq Frequency) QOption {
+	return func(q *Q) {
+		q.query.Set("collapse", string(freq))
+	}
+}
+
+// Frequency of the samples.
+type Frequency string
+
+// Frequency constants.
+const (
+	Daily     Frequency = "daily"
+	Weekly    Frequency = "weekly"
+	Monthly   Frequency = "montly"
+	Quarterly Frequency = "quarterly"
+	Annual    Frequency = "annual"
+)
+
+// Transfom defines transformations
+// to apply to data.
+type Transform string
+
+// Transform constants.
+const (
+	Delta         Transform = "diff"
+	DeltaPercent  Transform = "rdiff"
+	CumulativeSum Transform = "cumul"
+	Base100       Transform = "normalize"
+)
+
+// OptionLimit sets the number of dataset returned.
+// Set limit to 1 to return the last dataset.
+func OptionLimit(n int) QOption {
+	return func(q *Q) {
+		q.query.Set("limit", fmt.Sprintf("%d", n))
+	}
 }
