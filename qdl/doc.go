9cdb8074f8c1f6a67d76d490bb3ee18bfcb78e73
xavier268 <xavier@Fedora31OnDellXPS>
2020-01-16 11:48:54 +0100

    cleaned up old versions

--- qdl/doc.go
+++
@@ -1,7 +0,0 @@
-// Package qdl provides access to the quandl web services.
-// It uses the API key, "APISecretKey", that is defined
-// as a string constant in the secret.go file
-// (ignored by git).
-package qdl
-
-const APISecretKey = "secretKeyValue"
