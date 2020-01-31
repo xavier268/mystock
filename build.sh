6c4efe74b0ad3e192bbf890ce1d9c46eb3039a57
xavier268 <xavier@Fedora31OnDellXPS>
2020-01-31 11:23:46 +0100

    new binaries

--- build.sh
+++ build.sh
@@ -11,8 +11,6 @@ go build -o ${MYBIN}mystock $BASE/cmd/mystock/
 chmod +x ${MYBIN}mystock
 
 echo "Building windows version"
-GOOS=windows
-GOARCH=386
-go build -o ${MYBIN}mystock.exe $BASE/cmd/mystock/
+GOOS=windows GOARCH=amd64 go build -o ${MYBIN}mystock.exe $BASE/cmd/mystock/
 chmod +x ${MYBIN}mystock
 
