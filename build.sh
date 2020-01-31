6bd39cfb51e568abfb8619fe1330abc02e80b38b
xavier268 <xavier@Fedora31OnDellXPS>
2020-01-31 11:09:40 +0100

    cross-build windows & linux

--- build.sh
+++ build.sh
@@ -4,9 +4,15 @@ BASE=$(pwd)
 BASE=${BASE/mystock*/}"mystock"
 echo "Base folder detected as :" $BASE
 MYBIN=$BASE/bin/
-
-
 mkdir -p $MYBIN
-go build -o ${MYBIN} $BASE/... 
+
+echo "Building linux version"
+go build -o ${MYBIN}mystock $BASE/cmd/mystock/
+chmod +x ${MYBIN}mystock
+
+echo "Building windows version"
+GOOS=windows
+GOARCH=386
+go build -o ${MYBIN}mystock.exe $BASE/cmd/mystock/
 chmod +x ${MYBIN}mystock
 
